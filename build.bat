16e210fae4dc15ed4ee8540e7c3ab2f13f254aed
Nock.CS <87223282+NickPlayzGITHUB@users.noreply.github.com>
2025-10-09 03:46:38 -0600

    removed deprecated stuff

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Build the .exe using PyInstaller
-pyinstaller --onefile --noconsole CrossPatch.py
-pyinstaller --onefile --noconsole --icon=CrossP.ico CrossPatch.py
-
-
-REM After building, the .exe will be in the "dist" folder
-echo Build complete! Your exe is in the dist folder.
-pause
